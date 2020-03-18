862e97d2cc37c8bf39c1cc6de709ae499c51b408
Neil Patrick Lim de los Reyes <neil.delosreyes@obf.ateneo.edu>
2020-03-18 15:20:47 +0800

    adding templates and froyo and selenium to the settings

--- thegoodplace/thegoodplace/settings.py
+++ thegoodplace/thegoodplace/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'selenium',
+	'froyo',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'thegoodplace.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'heroes', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
