fdd38349562a9fa9e2488f4d70d2c5b25682e0cb
Neil Patrick Lim de los Reyes <neil.delosreyes@obf.ateneo.edu>
2020-03-18 15:47:41 +0800

    adding html to templates folder, html to urls, defining requests to show html in views

--- thegoodplace/froyo/urls.py
+++ thegoodplace/froyo/urls.py
@@ -2,5 +2,18 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'list/', views.list)
+    url(r'ingredients_list/', views.ingredients_list),
+    url(r'ingredients_detail/', views.ingredients_detail),
+    url(r'ingredients_update_form/', views.ingredients_update_form),
+    url(r'ingredients_create_form/', views.ingredients_create_form),
+
+    url(r'recipes_list/', views.recipes_list),
+    url(r'recipes_detail/', views.recipes_detail),
+    url(r'recipes_update_form/', views.recipes_update_form),
+    url(r'recipes_create_form/', views.recipes_create_form),
+
+    url(r'orders_list/', views.orders_list),
+    url(r'orders_detail/', views.orders_detail),
+    url(r'orders_update_form/', views.orders_update_form),
+    url(r'orders_create_form/', views.orders_create_form),
 ]
--- thegoodplace/froyo/views.py
+++ thegoodplace/froyo/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
-def list(request):
+def ingredients_list(request):
     return render(request, 'ingredients_list.html')
+def ingredients_detail(request):
+    return render(request, 'ingredients_detail.html')
+def ingredients_update_form(request):
+    return render(request, 'ingredients_update_form.html')
+def ingredients_create_form(request):
+    return render(request, 'ingredients_create_form.html')
+
+def recipes_list(request):
+    return render(request, 'recipes_list.html')
+def recipes_detail(request):
+    return render(request, 'recipes_detail.html')
+def recipes_update_form(request):
+    return render(request, 'recipes_update_form.html')
+def recipes_create_form(request):
+    return render(request, 'recipes_create_form.html')
+
+def orders_list(request):
+    return render(request, 'orders_list.html')
+def orders_detail(request):
+    return render(request, 'orders_detail.html')
+def orders_update_form(request):
+    return render(request, 'orders_update_form.html')
+def orders_create_form(request):
+    return render(request, 'orders_create_form.html')
