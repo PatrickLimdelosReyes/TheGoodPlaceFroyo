95dc56b6851b845193e2edbe77106ba4a1f83c33
Neil Patrick Lim de los Reyes <neil.delosreyes@obf.ateneo.edu>
2020-03-18 16:05:02 +0800

    starting to work on tests.py for a new customer commit

--- thegoodplace/froyo/tests.py
+++ thegoodplace/froyo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import unittest
+from django.urls import resolve
+from selenium import webdriver
 
-# Create your tests here.
+class CustomerTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
