f03d4ecfbf683a9673d0a5eaf24ad203965ffaca
Neil Patrick Lim de los Reyes <neil.delosreyes@obf.ateneo.edu>
2020-03-18 16:26:16 +0800

    added two tests, added to .gitignore, so far tests works commit

--- thegoodplace/froyo/tests.py
+++ thegoodplace/froyo/tests.py
@@ -9,3 +9,16 @@ class CustomerTest(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
+		
+    def test_can_display_ingredients_list(self):
+        self.browser.get('http://localhost:8000/froyo/ingredients_list/')
+        correct_value = 'Ingredients - List'
+        self.assertIn(correct_value, self.browser.title)
+        detail = self.browser.find_element_by_tag_name('h1')
+        self.assertEqual(detail.get_attribute('innerHTML'), correct_value)
+
+    def test_can_display_all_froyo_pages(self):
+        self.fail('Finished the test! Test Successful!')		
+		
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')		
